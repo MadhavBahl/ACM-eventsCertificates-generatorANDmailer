c465be48bfd4efb8185b3a4567ab6b6b836db71d
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-24 19:17:04 +0530

    Use JIMP to Write text on images

---
+++ playground/testGM.js
@@ -0,0 +1,38 @@
+// var fs = require('fs')
+//   , gm = require('gm').subClass({imageMagick: true});
+//
+//   gm('/base.jpg')
+//   .flip()
+//   .magnify()
+//   .rotate('green', 45)
+//   .blur(7, 3)
+//   .crop(300, 300, 150, 130)
+//   .edge(3)
+//   .write('/crazy.jpg', function (err) {
+//     if (!err) console.log('crazytown has arrived');
+//     else console.log(err);
+//   })
+
+var Jimp = require("jimp");
+
+// open a file called "lenna.png"
+// Jimp.read("ai.png", function (err, lenna) {
+//     if (err) throw err;
+//     lenna.resize(800, 1200)            // resize
+//          .write("aiOut.jpg"); // save
+// });
+
+var str = 'Madhav Bahl';
+var x = 300;
+var y = 487;
+
+Jimp.read("ai.png", function (err, image) {
+  Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) { // load font from .fnt file
+      image.print(font, x, y, str).write("aiOut.jpg");        // print a message on an image
+  });
+});
+
+// Jimp.loadFont( path ).then(function (font) { // load font from .fnt file
+//     image.print(font, x, y, str);        // print a message on an image
+//     image.print(font, x, y, str, width); // print a message on an image with text wrapped at width
+// });
